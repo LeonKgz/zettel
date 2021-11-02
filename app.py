269db85d255fbc71fd025925e26b04a91ee43c04
Albanec671 <root@guest.guest>
2021-11-02 12:39:27 +0000

    added a page for the social credit table

--- app.py
+++ app.py
@@ -3,9 +3,86 @@ from flask import request
 
 import os
 import re
+import pymysql.cursors
+from dotenv import load_dotenv
+
+load_dotenv()
+
+HOST = str(os.getenv('DB_HOST'))
+USER = str(os.getenv('DB_USER'))
+PASSWORD = str(os.getenv('DB_PASSWORD'))
+DB = str(os.getenv('DB_DATABASE'))
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
+
+def get_db_cursor():
+  db = pymysql.connect(host=HOST,
+                       user=USER,
+                       password=PASSWORD,
+                       db=DB,
+                       charset='utf8mb4',
+                       cursorclass=pymysql.cursors.DictCursor)
+  return db, db.cursor()
+
+@app.route('/score')
+def score():
+
+  db, cursor = get_db_cursor()
+  sql = f"SELECT * FROM raiting"
+
+  try:
+    cursor.execute(sql)
+    entries = cursor.fetchall()
+    entries = sorted([(e["Name"], e["Points"], e["Confession"]) for e in entries], key=lambda x: x[1], reverse=True)
+
+    string = "\n".join([f"<tr> <th>{i + 1}</th> <th>{name}</th> <th>{confession}</th> <th>{points}</th> </tr>" for i, (name, points, confession) in enumerate(entries)])
+    ret = f"""<html lang='ru'>
+								<style>
+									th, td {{
+										border:1px solid grey;
+										font-size: 150%;
+                    font-weight: normal;
+									}}
+
+									table {{
+										margin: 40px 0px 0px 0px;
+									}}
+
+									body, title {{
+										background-color: #282a36;
+										font-size: 150%;
+										color: #f8f8f2;
+										color: #f2f2f2;
+										font-family: Source Sans Pro;
+                    display: block;
+										font-weight: bold;
+									}}                 
+  
+								</style>
+
+								<body>
+									<center>
+                  <title>Социальный Рейтинг ТМГ</title>
+										<table>
+											<tr> 
+												<th> # </th> <th> Name </th> <th> Description </th> <th> Social Credit </th> 
+											</tr> 
+
+											{string}
+
+										</table>
+									</center>
+								</body>
+							</html>"""
+
+  except Exception as e:
+    ret = f"<h1>Ошибка подключения к базе данных! => {e} <=</h1>" 
+    print(e)
+    db.rollback()
+  db.close()
+  
+  return ret
 
 @app.route('/remedies')
 def remedies():
